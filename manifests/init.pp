d215013679316ee73b193c105734b554f32f3540
Marc Villacorta <marc.villacorta@gmail.com>
2013-04-21 20:37:12 +0200

    Adding hostgroup fact.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,28 @@
-class factsdotd {
+class factsdotd ( $hostgroup = false ) {
 
+    #------------
+    # Directory:
+    #------------
+
+    file { ['/etc/facter','/etc/facter/facts.d']:
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+
+    }
+
+    #------------
+    # hostgroup:
+    #------------
+
+    if $hostgroup == true {
+        file { '/etc/facter/facts.d/hostgroup.sh':
+            ensure  => present,
+            content => template("${module_name}/${operatingsystem}/hostgroup.sh.erb"),
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0755',
+        }
+    }
 }
